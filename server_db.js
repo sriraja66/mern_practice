c75a555107e80231157142ea60e546a28d8ab4e6
sriraja66 <srirajavadivel8@gmail.com>
2026-04-20 23:55:47 +0530

    Commiting changes to mern_practice repository

--- server_db.js
+++ server_db.js
@@ -1,20 +1,23 @@
-import express from 'express';
-import { MongoClient } from 'mongodb';
+import express from "express";
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.json());
 
-const url = "mongodb+srv://srirajavadivel:sriraj123@cluster0.hogh0pd.mongodb.net/?retryWrites=true&w=majority";
+const url =
+  "mongodb+srv://srirajavadivel:sriraj123@cluster0.hogh0pd.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(url);
-    const db = client.db("studentDB");
-    const studentscollection = db.collection("students");
+
+let studentsCollection; // global variable
 
 async function startServer() {
   try {
-
     await client.connect();
     console.log("MongoDB connected");
+
+    const db = client.db("studentDB");
+    studentsCollection = db.collection("students");
 
     app.listen(3000, () => {
       console.log("Server running on port 3000");
